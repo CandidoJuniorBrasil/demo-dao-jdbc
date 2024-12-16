44832f2e3bd06a4243f76dfe2a171a38adf8a1b7
Candido Jr <cand2005@gmail.com>
2024-12-16 05:47:37 -0300

    Classe entidade Vendedor

--- src/Aplicacao/ExecutaODBC.java
+++ src/Aplicacao/ExecutaODBC.java
@@ -1,13 +1,19 @@
 package Aplicacao;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class ExecutaODBC {
 
 	public static void main(String[] args) {
 	
 		Departamento dpt = new Departamento(1,"Livros");
-		System.out.println(dpt);
+		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dpt);
+		
+		System.out.println(vendedor);
 
 	}
 
